c3550500548b773228c504d8bc26034b39e8e9b3
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-07-12 20:04:07 -0500

    thought routes

---
+++ controllers/thoughtController.js
@@ -0,0 +1,68 @@
+const { Thought, User } = require('../models');
+
+module.exports = {
+    // get all thoughts
+    async getAllThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find();
+            res.json(thoughts);
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    },
+    // get one thought by id
+    async getSingleThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId });
+
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with this id!' });
+            }
+            res.json(thought);
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    },
+    // create a thought
+    async createThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+            const user = await User.findOneAndUpdate(
+                { username: req.body.username },
+                { $push: { thoughts: thought._id } },
+                { new: true }
+            );
+            if (!user) {
+                return res.status(404).json({ message: 'No user with this id!' });
+            }
+            res.json(thought);
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    },
+    // update a thought by id
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndUpdate(
+                { _id: req.params.thoughtId },
+                req.body,
+                { new: true, runValidators: true }
+            );
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    },
+    // delete a thought by id
+    async deleteThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+
+            if (!thought) {
+                return res.status(404).json({ message: 'No thought with this id!' });
+            }
+            res.json(thought);
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    }
+}
