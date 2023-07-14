dd33f0c6b61e6c8a6114c571c1e9a5f4ef3a3e0c
Matt Chiaro <131219830+MattChiaro@users.noreply.github.com>
2023-07-13 21:43:24 -0500

    basic seed

--- utils/seed.js
+++ utils/seed.js
@@ -22,24 +22,6 @@ connection.once('open', async () => {
   await User.insertMany(users);
   console.log('Users seeded successfully');
 
-  // // Create thoughts for each user
-  // for (let i = 0; i < 10; i++) {
-  //   const user = users[i];
-  //   const thoughts = [];
-  //   const numThoughts = Math.floor(Math.random() * 3) + 3; // Random number of thoughts between 3 and 5
-
-  //   for (let j = 0; j < numThoughts; j++) {
-  //     thoughts.push({
-  //       thoughtText: `Thought ${j + 1} by ${user.username}`,
-  //       username: user.username,
-  //       reactions: [], // Empty reactions array for now
-  //     });
-  //   }
-
-  //   await Thought.insertMany(thoughts);
-  //   console.log(`Thoughts seeded successfully for user ${user.username}`);
-  // }
-
   console.log('Seeding complete!');
   process.exit(0);
 });
